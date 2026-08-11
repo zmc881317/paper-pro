ac1609a3ce2abb5f969740c2436a9fd8dfb6895e
zmc881317 <146724712+zmc881317@users.noreply.github.com>
2026-08-11 09:33:09 +0800

    Change default HY2_PORT from 24157 to 24079

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -53,7 +53,7 @@ public class App {
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
-    private static final String HY2_PORT = env("HY2_PORT", "24157");
+    private static final String HY2_PORT = env("HY2_PORT", "24079");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "cf.877774.xyz");
